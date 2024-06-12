f9150173ed6f07382584ca0fdd30c171942f6239
TwanWolthaus <twanwolthaus@gmail.com>
2024-06-12 08:41:25 +0000

    reset change

--- database/migrations/2024_06_12_071527_create_medal_table.php
+++ database/migrations/2024_06_12_071527_create_medal_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('medal', function (Blueprint $table) {
             $table->id();
-            $table->name(string);
             $table->timestamps();
         });
     }
