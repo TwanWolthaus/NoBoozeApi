cc9614a7c6d5613a6a59e453984d17daac8384cf
TwanWolthaus <twanwolthaus@gmail.com>
2024-06-12 15:31:37 +0000

    Created the start of an API that loads in medals.

--- app/Models/Medal.php
+++ app/Models/Medal.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Medal extends Model
 {
     use HasFactory;
+
+    protected $table = 'medal';
+
+    public function userMedals(): HasMany
+    {
+        return $this->hasMany(UserMedal::class);
+    }
 }
--- resources/views/other.php
+++
@@ -1,5 +0,0 @@
-<html>
-    <body>
-        <h1>Hallo Titus :)</h1>
-    </body>
-</html>
