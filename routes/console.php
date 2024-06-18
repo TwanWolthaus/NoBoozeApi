5c7d46ed34c9b80c5bd46a08d938372695073329
TwanWolthaus <twanwolthaus@gmail.com>
2024-06-18 18:04:02 +0000

    Feat: daily update on streak and dry days implemented on console. Feat: added crontab to server to run this.

--- routes/console.php
+++ routes/console.php
@@ -2,8 +2,19 @@
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
- 
-$schedule->call(function () {
-    DB::table('users')->increment('streak');
+use Illuminate\Support\Facades\Artisan;
+
+
+// IMPORTANT!
+//
+// If this is not working, run 'php artisan schedule:list'.
+// If it shows '* * * * * 1d  php artisan increment:dry-days', make sure you have a crontab enabled that runs this schedule.
+
+Artisan::command('increment:dry-days', function () {
+  
     DB::table('users')->increment('dry_days');
-})->everyMinute();
+    DB::table('users')->where('streak', 0)->decrement('dry_days');
+    DB::table('users')->increment('streak');
+
+})->daily()->runInBackground();
+
